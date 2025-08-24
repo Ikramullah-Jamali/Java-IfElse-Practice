6dfb182a392bee103ee02202f0f338d6111500fa
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-08-24 15:43:39 +0500

    Create PrimeNumberTest_WithUserInput.java

---
+++ Check Prime Nubers/PrimeNumberTest_WithUserInput.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Scanner;
+class PrimeNumberTest_WithUserInput 
+{
+    public static void main(String [] args)
+    {
+          
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int num=s.nextInt();
+        int temp=0;
+        if (num <= 1) 
+        {
+            temp = 1; // not a prime
+        } 
+        else 
+        {
+            for (int i = 2; i <= num - 1; i++) 
+            {
+                if (num % i == 0) 
+                {
+                    temp = temp + 1;
+                }
+            }
+        }
+
+        if (temp == 0) 
+        {
+            System.out.println(num + " is prime number.");
+        } 
+        else 
+        {
+            System.out.println(num + " is not a prime number.");
+        }
+        s.close();
+    }
+}
