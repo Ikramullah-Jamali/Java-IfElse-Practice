e9e283445484c78d5225a9cd796163a98d84b9b4
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-08-24 15:44:49 +0500

    Create PrimeNumberTest_WithoutUserInput.java

---
+++ Check Prime Nubers/PrimeNumberTest_WithoutUserInput.java
@@ -0,0 +1,34 @@
+
+
+class PrimeNumberTest_WithoutUserInput
+{
+    public static void main(String [] args)
+    {
+        int num=7;
+        int temp=0;
+
+        if (num <= 1) 
+        {
+            temp = 1; // to indicate it's not prime
+        } 
+        else 
+        {
+            for (int i = 2; i <= num - 1; i++) 
+            {
+                if (num % i == 0) 
+                {
+                    temp = temp + 1;
+                }
+            }
+        }
+
+        if (temp == 0) 
+        {
+            System.out.println(num + " is prime");
+        } 
+        else 
+        {
+            System.out.println(num + " is not a prime number");
+        }
+       }
+}
